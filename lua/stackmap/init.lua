76cec99b82be47c912bb23a4d383250381f49f99
Luis Henrique <luis.medeiros@institutojef.org.br>
2026-07-06 17:38:47 -0300

    feat(lua/stackmap): implement logic to pop mapping when not existing

--- lua/stackmap/init.lua
+++ lua/stackmap/init.lua
@@ -1,20 +1,15 @@
-P = function(v)
-  print(vim.inspect(v))
-  return v
-end
-
 -- It's like a Module
 -- M is a table, but used as a Module
 local M = {}
 
 local find_mapping = function(maps, lhs)
-  for _, value in ipairs(maps) do
-    if value.lhs == lhs then
-      return value
-    end
-  end
+	for _, value in ipairs(maps) do
+		if value.lhs == lhs then
+			return value
+		end
+	end
 
-  return nil
+	return nil
 end
 
 M._stack = {}
@@ -23,30 +18,46 @@ M._stack = {}
 ---@param mode string
 ---@param mappings table
 M.push = function(name, mode, mappings)
-  local maps = vim.api.nvim_get_keymap(mode)
+	local maps = vim.api.nvim_get_keymap(mode)
 
-  local existing_maps = {}
+	local existing_maps = {}
 
-  for lhs, rhs in pairs(mappings) do
-    local existing = find_mapping(maps, lhs)
-    if existing then
-      table.insert(existing_maps, existing)
-    end
-  end
+	for lhs, _ in pairs(mappings) do
+		local existing = find_mapping(maps, lhs)
+		if existing then
+			existing_maps[lhs] = existing
+		end
+	end
 
-  M._stack[name] = existing_maps
+	M._stack[name] = {
+		mode = mode,
+		existing = existing_maps,
+		mappings = mappings,
+	}
 
-  for lhs, rhs in pairs(mappings) do
-    -- TODO: need some way to pass options in here
-    vim.keymap.set(mode, lhs, rhs)
-  end
+	for lhs, rhs in pairs(mappings) do
+		-- TODO: need some way to pass options in here
+		vim.keymap.set(mode, lhs, rhs)
+	end
 end
 
-M.pop = function(mode) end
+---@param name string
+M.pop = function(name)
+	local state = M._stack[name]
+	M._stack[name] = nil
+
+	for lhs, rhs in pairs(state.mappings) do
+		if state.existing[lhs] then
+			-- TODO: Implement logic when maps existing
+		else
+			vim.keymap.del(state.mode, lhs, rhs)
+		end
+	end
+end
 
 M.push("debug_mode", "n", {
-  [" h"] = "echo 'Hello'",
-  [" e"] = "echo 'Goodbye'",
+	[" h"] = "echo 'Hello'",
+	[" e"] = "echo 'Goodbye'",
 })
 
 return M
